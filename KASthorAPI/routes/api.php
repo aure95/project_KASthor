8ba5bca10b9238e1577c2ce2586f3feb5a8f6f89
aurelien <aurelien.mouret@imie-paris.fr>
2022-06-06 17:01:31 +0200

    initial commit

---
+++ KASthorAPI/routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Resources\CategoryCollections;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/categories', function () {
+    return CategoryCollections::collection(Category::all());
+});
+
+Route::post('/categories/{name}', function ($name) {
+     $category = new Category();
+     $category->name = $name;
+     $category->save();
+});
