fbdff4b15622cf716f031fa4fd9134def4f7c2b1
aurelien <aurelien.mouret@imie-paris.fr>
2022-06-08 22:12:29 +0200

    created Tag model and mongoDB Schema

---
+++ KASthorAPI/database/migrations/2022_06_08_193937_create_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    protected $connection = 'mongodb';
+
+    public function up()
+    {
+        Schema::create('Tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('value');
+;        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Tags');
+    }
+}
