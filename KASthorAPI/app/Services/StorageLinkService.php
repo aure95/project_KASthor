fd9503a66eff85df173699db18307012e1bc66be
aurelien <aurelien.mouret@imie-paris.fr>
2022-06-20 11:29:12 +0200

    cleaned StorageLinkService

--- KASthorAPI/app/Services/StorageLinkService.php
+++ KASthorAPI/app/Services/StorageLinkService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Google\Cloud\Storage\StorageClient;
+use App\Models\StorageLink;
 
 class StorageLinkService
 {
@@ -10,27 +11,24 @@ class StorageLinkService
     private $STORAGE_LINK_BUCKET_NAME = 'kasthorapp.appspot.com';
 
     public function __construct() {
-
         // Authenticating with keyfile data.
         $this->storage = new StorageClient([
             'keyFile' => json_decode(file_get_contents($this->FIREBASE_CONFIG_JSON_PATH), true)
         ]);
     }
 
-    //to clean
+    public function uploadStorageLink(StorageLink $storageLink ,String $filePathToSave) {
+        $this->upload($storageLink->name);
+    }
+
+    public function downloadStorageLink(StorageLink $storageLink ,String $filePathToSave) {
+        $this->download($storageLink->name, $filePathToSave);
+    }
+
     public function download(String $fileNameWithExtension, String $filePathToSave) {
-
-        // foreach ($buckets as $bucket) {
-        //     echo $bucket->name() . PHP_EOL;
-        // };
-
         $bucket = $this->storage->bucket($this->STORAGE_LINK_BUCKET_NAME);
         $object = $bucket->object($fileNameWithExtension);
         $object->downloadToFile($filePathToSave);
-        // $this->storage->getBucket();
-        // $object = $bucket->upload($file, [
-        //     'name' => "Pictures"
-        // ]);
     }
 
     public function upload(String $filePath) {
@@ -41,15 +39,5 @@ class StorageLinkService
                 'predefinedAcl' => 'publicRead'
             ]
         );
-        // $buckets = $this->storage->buckets();
-
-        // foreach ($buckets as $bucket) {
-        //     echo $bucket->name() . PHP_EOL;
-        // };
-
-        // $this->storage->getBucket();
-        // $object = $bucket->upload($file, [
-        //     'name' => "Pictures"
-        // ]);
     }
 }
