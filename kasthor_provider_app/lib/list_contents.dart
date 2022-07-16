0e58a4097ac4b5c55b415eb4db5e3ff33dc7d705
aurelien <aurelien.mouret@imie-paris.fr>
2022-07-16 11:48:56 +0200

    updated kist_content, added TextFormField, dateFormPicker and form and some validator

--- kasthor_provider_app/lib/list_contents.dart
+++ kasthor_provider_app/lib/list_contents.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import '../services/content_service.dart';
 import '../models/content.dart';
 import 'package:flutter/foundation.dart';
+import './content_form.dart';
 
 
 void main() {
@@ -47,20 +48,6 @@ class MyApp extends StatelessWidget {
     List<Tab> tabsGenerated = getTabs(userRole); 
 
     return MaterialApp(
-      
-      // title: 'manage contents',
-      // theme: ThemeData(
-      //   // This is the theme of your application.
-      //   //
-      //   // Try running your application with "flutter run". You'll see the
-      //   // application has a blue toolbar. Then, without quitting the app, try
-      //   // changing the primarySwatch below to Colors.green and then invoke
-      //   // "hot reload" (press "r" in the console where you ran "flutter run",
-      //   // or simply save your changes to "hot reload" in a Flutter IDE).
-      //   // Notice that the counter didn't reset back to zero; the application
-      //   // is not restarted.
-      //   primarySwatch: Colors.blue,
-      // ),
       home: DefaultTabController(
         length: tabsGenerated.length,
         child: Scaffold(
@@ -76,7 +63,7 @@ class MyApp extends StatelessWidget {
         body: const TabBarView(
             children: [
               const ManageContentPage(),
-              Icon(Icons.directions_transit),
+              const MyCustomForm() ,
               Icon(Icons.directions_bike),
             ],
           ),
@@ -88,15 +75,6 @@ class MyApp extends StatelessWidget {
 
 class ManageContentPage extends StatefulWidget {
   const ManageContentPage({Key? key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   State<ManageContentPage> createState() => _ManageContentState();
@@ -118,31 +96,11 @@ class _ManageContentState extends State<ManageContentPage> {
         
     }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+ 
     return Scaffold(
-      // appBar: AppBar(
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
+
       body: FutureBuilder<List<Content>>(
   future: contents,
   builder: (context, snapshot) {
